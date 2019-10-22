ad4bc31079a1bcaf8150b84cde98c8389e24a9d1
marcelo <fotofacil@live.com>
2019-10-21 22:32:19 -0300

    criarTabelaEstado com problemas de sintax

---
+++ criarTabelaEstado.sql
@@ -0,0 +1,11 @@
+--criando tabela estados 
+CREATE TABLE estados(
+    id INT UNSIGNED not NULL AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    sigla VARCHAR(2) NOT NULL,
+    regiao ENUM('norte','nordeste','centro-Oeste', 'sudeste', 'sul') NOT NULL,
+    populacao DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE KEY(nome),
+    UNIQUE KEY(sigla),
+);

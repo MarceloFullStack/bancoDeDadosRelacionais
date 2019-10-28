6e217aada63e33eadba8b3e4cb9befeda0029eac
marcelo <fotofacil@live.com>
2019-10-28 14:40:36 -0300

    Realizando testes inserindo ID de forma forcada

---
+++ 6-inserirNovosEstados.sql
@@ -0,0 +1,12 @@
+--inserindo um estado de teste 
+insert into `estados`(id, nome,sigla,regiao,populacao)
+values(1000,'Novo','NV','Sul',2.45)
+
+
+--inserindo segundo estado de teste 
+insert into `estados`(nome,sigla,regiao,populacao)
+values('Novo de Novo','NC','Nordeste',5.45)
+
+
+
+select * from `estados`

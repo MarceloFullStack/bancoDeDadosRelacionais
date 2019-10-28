4caec5a2b23f6a346d7a8c526a0bb59ad4ed0672
marcelo <fotofacil@live.com>
2019-10-28 14:50:34 -0300

    deletando baseado em condicoes

---
+++ 7-deletarEstado.sql
@@ -0,0 +1,6 @@
+--deletando estado escolhendo como condicao uma deteminada sigla
+DELETE  FROM `estados` WHERE sigla ='NV'
+
+
+--deletando estado tendo com condicao o id ser maior ou igual a 1000
+DELETE  FROM `estados` WHERE id>=1000
